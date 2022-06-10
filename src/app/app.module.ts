f8ec7ba111e534b58129c5bb5ce8bc5e4b76511f
Lucas <lucasdec88@gmail.com>
2022-06-10 12:16:40 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
