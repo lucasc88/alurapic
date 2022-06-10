55722ad7c6a82ef45d49e3a5eef6421b9fb1283a
Lucas <lucasdec88@gmail.com>
2022-06-10 16:14:04 +0100

    Routes of components has been implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PhotoListComponent
+  },
+  {
+    path: 'p', component: PhotoFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
