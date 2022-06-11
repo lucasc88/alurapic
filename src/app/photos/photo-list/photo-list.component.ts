ba8902e1bdc6d595fad4346d3b792395722a7b51
Lucas <lucasdec88@gmail.com>
2022-06-11 14:06:13 +0100

    Load Btn implementation to load more photos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -16,24 +17,47 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();//Subject is a kind of Observable that remains alive
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
 
   ngOnInit(): void {
     //the resolver is used here to load photos firstly, before the component is rendered.
     //data['photosResolver'] is the property defined in the AppRouting.module.ts.
     this.photos = this.activatedRoute.snapshot.data['photosResolver'];
 
-    console.log('ngOnInit');
+    this.userName = this.activatedRoute.snapshot.params.userName;
+
     //Using debounceTime, it will wait 300 milliseconds to get what the user types
     this.debounce
       .pipe(debounceTime(300))
       .subscribe(filter => {
-        this.filter = filter;});
+        this.filter = filter;
+      });
   }
 
 
   //To kill the debounce to free up memory avoind memory leaking
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    //++ because the second time need to be 2, so a pre increment is necessary
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        //concat the new photos from the backend to the this.photos
+        //in this way, the OnChanges() in photos.component will get this modification.
+        //Angular only detect a modification in a @Input() when there is a new assignment in it
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {//when no more photos
+          this.hasMore = false;
+        }
+      });
   }
 }
