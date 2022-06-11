cbb0f406ba5704a1cb0503606804ed2664b8fea9
Lucas <lucasdec88@gmail.com>
2022-06-11 13:24:27 +0100

    debounce to request after 300 milli

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -7,10 +10,11 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();//Subject is a kind of Observable that remains alive
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -18,6 +22,18 @@ export class PhotoListComponent implements OnInit {
     //the resolver is used here to load photos firstly, before the component is rendered.
     //data['photosResolver'] is the property defined in the AppRouting.module.ts.
     this.photos = this.activatedRoute.snapshot.data['photosResolver'];
+
+    console.log('ngOnInit');
+    //Using debounceTime, it will wait 300 milliseconds to get what the user types
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => {
+        this.filter = filter;});
   }
 
+
+  //To kill the debounce to free up memory avoind memory leaking
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
