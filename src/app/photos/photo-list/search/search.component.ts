c5d14536ab706588d462f62775eab7d25f8d1907
Lucas <lucasdec88@gmail.com>
2022-06-13 11:05:10 +0100

    Custom event communicating with father component

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
 import { Subject } from "rxjs";
 import { debounceTime } from "rxjs/operators";
 
@@ -8,12 +8,15 @@ import { debounceTime } from "rxjs/operators";
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
+    @Output() onTyping = new EventEmitter<string>();//Custom event
+
     debounce: Subject<string> = new Subject<string>();//Subject is a kind of Observable that remains alive
 
     ngOnInit(): void {
         //Using debounceTime, it will wait 400 milliseconds to get what the user types
         this.debounce
-            .pipe(debounceTime(400));
+            .pipe(debounceTime(400))
+            .subscribe(input => this.onTyping.emit(input));
     }
 
     //To kill the debounce to free up memory avoind memory leaking
