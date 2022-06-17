468c4be3b8881fb00880fd44cb0c40a186354b06
Lucas <lucasdec88@gmail.com>
2022-06-17 19:19:06 +0100

    Form validations implementation

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   //selector: 'app-signin', //it's unnecessary 'selector' because this component won't be used in other template component
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  //in the template is used the directive [formGroup]
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      //in the template is used the directive formControlName
+      userName: ['', Validators.required],//first position is the value, second is the validators
+      password: ['', Validators.required]
+    });
   }
 
 }
