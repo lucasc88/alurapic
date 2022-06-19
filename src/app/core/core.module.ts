134f7db0b678f7f211dc19bd8bf5530a6efcbe2b
Lucas <lucasdec88@gmail.com>
2022-06-19 13:34:27 +0100

    RouterLink implementation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
@@ -7,7 +8,8 @@ import { HeaderComponent } from "./header/header.component";
         HeaderComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule
     ],
     exports: [
         HeaderComponent
