ae7b97fffda573d131109ba4448ee25a284df281
Lucas <lucasdec88@gmail.com>
2022-06-30 23:18:22 +0100

    useHash to run in old browsers versions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],//useHash is to run in old browsers
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
