f7212a0318ce3bd313894b340d7ee6d497a78acb
Lucas <lucasdec88@gmail.com>
2022-06-30 18:06:20 +0100

    Assynchronous validation in the form

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
   //selector: 'app-signup', it will have a page scope, so is not necessary 
@@ -11,13 +11,19 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      userName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40),
-        lowerCaseValidator]],
+      userName:
+        [
+          '',//here is the default validator value
+          [Validators.required, Validators.minLength(2), Validators.maxLength(40)],//synchronous validators
+          this.userNotTakenValidatorService.checkUserNameTaken()//asynchronous validators
+        ],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     });
---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { AbstractControl } from "@angular/forms";
+import { debounceTime, switchMap, map, first } from "rxjs/operators";
+import { SignUpService } from "./signup.service";
+
+
+//It's an asynchronous validator to check if userName already exist.
+//It's a service and validator at the same time.
+//It's returns a function validation, an Observable of null or a JS object
+@Injectable({
+    providedIn: 'root'
+})
+export class UserNotTakenValidatorService {
+
+    constructor(private signUpService: SignUpService) { }
+
+    checkUserNameTaken() {
+        return (control: AbstractControl) => {
+            return control
+                .valueChanges//this is an Observable
+                .pipe(debounceTime(400))//debounceTime to request each 400 miliseconds
+                .pipe(switchMap(userName => //switchMap returns the other Observable(checkUserNameTaken)
+                    this.signUpService.checkUserNameTaken(userName)
+                ))
+                .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))//access the value returned by request
+                .pipe(first());//first() gets the first emmited value
+        }
+    }
+}
