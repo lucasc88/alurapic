f37e1ae31de1b29e7cc6cd2357d9f51b0d0b61ea
Lucas <lucasdec88@gmail.com>
2022-07-09 01:11:39 +0100

    PhotoService request in the PhotoDetails implemetd

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { Photo } from "../photo/photo";
+import { PhotoService } from "../photo/photo.service";
 
 //There is no selector because is a scope page component
 @Component({
-    templateUrl: './photo-details.component.html'
+    templateUrl: './photo-details.component.html',
+    styleUrls: ['./photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
+    photo$: Observable<Photo>;
 
-    constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private photoService: PhotoService
+    ) { }
 
     ngOnInit(): void {
         const id = this.activatedRoute.snapshot.params.photoId;
-        console.log('id: ' + id);
+        this.photo$ = this.photoService.findById(id);
     }
 
 }
