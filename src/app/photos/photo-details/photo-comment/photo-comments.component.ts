7fe6943ebc708427dbfd947c0ec646d16b3eca64
Lucas <lucasdec88@gmail.com>
2022-07-10 11:21:30 +0100

    Refresh comments after a POST comment process

--- src/app/photos/photo-details/photo-comment/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comment/photo-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -27,12 +28,22 @@ export class PhotoCommentsComponent implements OnInit {
     })
   }
 
-  save(){
+  //After the POST to add a comment, using the same Observable flow, the list of comments
+  //is updated by switchMap. SwitchMap changes the data flow from addComments to getComments
+  //and then, the subscribe will use the last data flow result (getComments()), so,
+  //just update the comments$.
+  //It doesn't work to make a POST and then a GET because they are asynchronous requests.
+  //One can run before the other.
+  save() {
     const comment = this.commentForm.get('comment').value;
-    this.photoService.addComment(this.photoId, comment)
-      .subscribe(() => {
+    this.comments$ = this.photoService
+      .addComment(this.photoId, comment)
+      //pipe to use the switchMap
+      .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+      //pipe to use tap(). Tap doesn't change the flow. It's usefull to run others procedures
+      .pipe(tap(() => {
         this.commentForm.reset();
-        alert('Photo registered successfully');
-      });
+        alert('Comment registered successfully');
+      }));
   }
 }
